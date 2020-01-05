447fa360d60551016ec1d2f4d479c08cb7f5a33e
Kuroki <isaacwang29@gmail.com>
2020-01-05 20:52:32 +0800

    feat: splash screen

--- app/src/main/java/com/example/osgeo/MainActivity.kt
+++ app/src/main/java/com/example/osgeo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.osgeo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var n = 0
+        tv_num.text = "$n"
+        
+        btn_add.setOnClickListener {
+            n++
+            tv_num.text = "$n"
+        }
     }
 }
