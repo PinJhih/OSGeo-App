30a9ac79e7f39aa546ec93c2204ad6523789bbb5
Kuroki <isaacwang29@gmail.com>
2020-01-06 18:48:22 +0800

    feat: recyclerView to show OSGeo projects

--- app/src/main/java/com/example/osgeo/MainActivity.kt
+++ app/src/main/java/com/example/osgeo/MainActivity.kt
@@ -2,20 +2,52 @@ package com.example.osgeo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var info = ArrayList<ProjectInfo>()
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var n = 0
-        tv_num.text = "$n"
-        
-        btn_add.setOnClickListener {
-            n++
-            tv_num.text = "$n"
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = RecyclerView.VERTICAL
+        recyclerView.layoutManager = linearLayoutManager
+        adapter = RecyclerAdapter(this, info)
+        recyclerView.adapter = adapter
+
+        setRecyclerView()
+    }
+    private fun setRecyclerView(){
+
+        val names = resources.obtainTypedArray(R.array.project_names)
+        val tagline = resources.obtainTypedArray(R.array.project_tagline)
+        val logos = resources.obtainTypedArray(R.array.project_logo_id)
+
+        for(i in 0 until 5){
+            val projectInfo = ProjectInfo()
+            projectInfo.name = names.getString(i)!!
+            projectInfo.tagline = tagline.getString(i)!!
+            projectInfo.logo_id = logos.getResourceId(i,0)
+            info.add(projectInfo)
+
+            adapter.notifyDataSetChanged()
         }
+
+        names.recycle()
+        tagline.recycle()
+        logos.recycle()
     }
 }
+
+
+data class ProjectInfo(
+    var name: String = "",
+    var tagline: String = "",
+    var logo_id: Int = 0
+)
---
+++ app/src/main/java/com/example/osgeo/RecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.osgeo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(private val context: Context, private val projectInfo: ArrayList<ProjectInfo>) :
+    RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val item = v.findViewById<ConstraintLayout>(R.id.layout_item)!!
+        val name = v.findViewById<TextView>(R.id.tv_project_name)!!
+        val tagline = v.findViewById<TextView>(R.id.tv_project_tagline)!!
+        val logo = v.findViewById<ImageView>(R.id.img_project_logo)!!
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
+        val v =
+            LayoutInflater.from(viewGroup.context).inflate(R.layout.item_projects, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount() = projectInfo.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.name.text = projectInfo[position].name
+        holder.tagline.text = projectInfo[position].tagline
+        holder.logo.setImageResource(projectInfo[position].logo_id)
+
+        holder.item.setOnClickListener {
+            Toast.makeText(context as MainActivity,projectInfo[position].name,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
